41159ef2e69c05f083d9fe23cb12399488e91e8c
hutchz72 <110713034+hutchz72@users.noreply.github.com>
2023-05-01 16:53:23 +0700

    Add files via upload

---
+++ LAB1.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package characterstream2;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author Student
+ */
+public class LAB1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        try {
+            FileWriter output = new FileWriter("data.txt");
+            output.write("Hello World!");
+            output.write("Welcome to Java");
+            output.close();
+        } catch (IOException ex) {
+            Logger.getLogger(LAB1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        try {
+            FileReader input = new FileReader("data.txt");
+            int ch;
+            while ((ch=input.read())!=-1){
+                System.out.println((char)ch);
+            }
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(LAB1.class.getName()).log(Level.SEVERE, null, ex);
+        } catch (IOException ex) {
+            Logger.getLogger(LAB1.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+}
